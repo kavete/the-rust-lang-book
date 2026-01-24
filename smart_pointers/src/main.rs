618cb4b8516b9d5a1d2961aefc8f396bab18df7b
kavete <kavetebrian@gmail.com>
2026-01-25 00:49:58 +0300

    Get strarted with smart pointers

---
+++ smart_pointers/src/main.rs
@@ -0,0 +1,69 @@
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`", self.data)
+    }
+}
+
+fn main() {
+    use List::{Cons, Nil};
+    let b = Box::new(5);
+    println!("b = {b}");
+
+    let x = 5;
+    let y = &x;
+    let z = Box::new(5);
+    let z2 = MyBox(5);
+
+    // println!("{y}");
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+    assert_eq!(5, *z);
+
+    assert_eq!(5, *z2);
+
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    println!("{:?}", list);
+
+    let c = CustomSmartPointer {
+        data: String::from("Declared First"),
+    };
+
+    let d = CustomSmartPointer {
+        data: String::from("Declared Second"),
+    };
+
+    println!("CustomSmartPointers created");
+
+    drop(d);
+    println!("Custom smart pointer dropped before the end of main");
+}
